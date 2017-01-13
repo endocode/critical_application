9917dd222b7cdb044f9b1f8e554e7d6fcc5bda7b
Matthias Schmitz <matthias@endocode.com>
2017-01-13 14:14:08 +0100

    Add critical application
    * This is a demo application for the bootime optimization lab.

---
+++ critical_application.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <errno.h>
+
+int main (void) {
+	FILE* led = NULL;
+
+	led = fopen("/tmp/sys/class/leds/d3/brightness", "w");
+	if (led == NULL) {
+		perror("Error: ");
+		return(1);
+	}
+	fprintf(led, "1");
+        fclose(led);
+	return(0);
+}
