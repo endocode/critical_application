a85933abab47c6a6cb7e428ef86ccf3c2cb34ab1
Matthias Schmitz <matthias@endocode.com>
2017-01-19 17:26:16 +0100

    Add debug output

--- critical_application.c
+++ critical_application.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <errno.h>
 
-int main (void) {
+int main (int argc, char *argv[]) {
 	FILE* led = NULL;
-
+	int DEBUG = 0;
+	if (argc > 1) {
+		DEBUG = 1;
+	}
+	if ( DEBUG == 1 ) {
+		printf("\nStarting critical application\n");
+	}
 	led = fopen("/sys/class/leds/d3/brightness", "w");
 	if (led == NULL) {
 		perror("Error: ");
@@ -11,5 +17,8 @@ int main (void) {
 	}
 	fprintf(led, "1");
         fclose(led);
+	if ( DEBUG == 1 ) {
+		printf("\nDone critical application\n");
+	}
 	return(0);
 }
